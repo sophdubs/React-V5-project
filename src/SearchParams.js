53b5d9f3287986ee998d6902ded42307123c85f9
Sophie Dubois <sophiemargueritedubois@gmail.com>
2019-07-09 14:09:04 +1000

    Create a generic 'Dropdown' hook to refactor the searchParams dic

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
 
 const SearchParams = () => {
   //Hooks never go inside of if statements or for loops */
   //Hooks always start with use. They also return the current state and a function to update that state
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("dog");
+  const [breed, setBreed] = useState("");
+  const [breeds, setBreeds] = useState([]);
+
   return (
     <div className="search-params">
       <form action="">
@@ -15,6 +20,39 @@ const SearchParams = () => {
             placeholder="Location"
             onChange={event => setLocation(event.target.value)}
           />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={event => setAnimal(event.target.value)}
+            onBlur={event => setAnimal(event.target.value)}
+          >
+            <option>All</option>
+            {ANIMALS.map(animal => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={event => setBreed(event.target.value)}
+            onBlur={event => setBreed(event.target.value)}
+            disabled={breeds.length === 0}
+          >
+            <option>All</option>
+            {breeds.map(breedString => (
+              <option key={breedString} value={breedString}>
+                {breedString}
+              </option>
+            ))}
+          </select>
         </label>
         <button>Submit</button>
       </form>
