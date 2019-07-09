c2c6f3f244bee982ccf2503397491148501cdb15
Sophie Dubois <sophiemargueritedubois@gmail.com>
2019-07-09 21:46:40 +1000

    Class component and config babel for parcel

--- src/Details.js
+++ src/Details.js
@@ -1,7 +1,47 @@
 import React from "react";
+import pet from "@frontendmasters/pet";
 
-const Details = props => {
-  return <h1>hi lol</h1>;
-};
+class Details extends React.Component {
+  state = { loading: true };
+  //same as below, but this top one needs babel
+  //   constructor(props) {
+  //     super(props);
+
+  //     this.state = {
+  //       loading: true
+  //     };
+  //   }
+  componentDidMount() {
+    pet.animal(this.props.id).then(({ animal }) => {
+      this.setState({
+        name: animal.name,
+        animal: animal.type,
+        location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
+        description: animal.description,
+        medial: animal.photos,
+        brees: animal.breeds.primary,
+        loading: false
+      });
+    }, console.error);
+  }
+  render() {
+    if (this.state.loading) {
+      return <h1>loading...</h1>;
+    }
+
+    const { animal, breed, location, description, name } = this.state;
+
+    return (
+      <div className="details">
+        <div>
+          <h1>{name}</h1>
+          <h2>{`${animal} - ${breed} - ${location}`}</h2>
+          <button>Adopt {name}</button>
+          <p>{description}</p>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Details;
