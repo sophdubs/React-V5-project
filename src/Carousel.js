12c052f34b2430e799cd33f7b0c1096d43d70954
Sophie Dubois <sophiemargueritedubois@gmail.com>
2019-07-10 11:07:54 +1000

    Added image carousel to details page so now clients can see a carousel of images of the dog they are requesting details of

--- src/Carousel.js
+++ src/Carousel.js
@@ -5,6 +5,23 @@ class Carousel extends React.Component {
     photos: [],
     active: 0
   };
+
+  static getDerivedStateFromProps({ media }) {
+    let photos = ["http://placecorgi.com/600/600"];
+
+    if (media.length) {
+      photos = media.map(({ large }) => large);
+    }
+
+    return { photos };
+  }
+
+  handleIndexClick = event => {
+    this.setState({
+      active: +event.target.dataset.index
+    });
+  };
+
   render() {
     const { photos, active } = this.state;
 
