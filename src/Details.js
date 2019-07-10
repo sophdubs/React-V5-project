654d21d82d2675aaee8217575310bf512bb5e3fd
Sophie Dubois <sophiemargueritedubois@gmail.com>
2019-07-10 14:31:59 +1000

    Updated app to have a dropdown to chose a theme

--- src/Details.js
+++ src/Details.js
@@ -2,6 +2,7 @@ import React from "react";
 import pet from "@frontendmasters/pet";
 import Carousel from "./Carousel";
 import ErrorBoundary from "./ErrorBoundary";
+import ThemeContext from "./ThemeContext";
 
 class Details extends React.Component {
   state = { loading: true };
@@ -21,7 +22,7 @@ class Details extends React.Component {
         location: `${animal.contact.address.city}, ${animal.contact.address.state}`,
         description: animal.description,
         media: animal.photos,
-        brees: animal.breeds.primary,
+        breed: animal.breeds.primary,
         loading: false
       });
     }, console.error);
@@ -39,7 +40,14 @@ class Details extends React.Component {
         <div>
           <h1>{name}</h1>
           <h2>{`${animal} - ${breed} - ${location}`}</h2>
-          <button>Adopt {name}</button>
+          <ThemeContext.Consumer>
+            {/* You could destructure the theme here by doing {([theme])}=> and then using backgroundColor: theme instead */}
+            {themeHook => (
+              <button style={{ backgroundColor: themeHook[0] }}>
+                Adopt {name}
+              </button>
+            )}
+          </ThemeContext.Consumer>
           <p>{description}</p>
         </div>
       </div>
